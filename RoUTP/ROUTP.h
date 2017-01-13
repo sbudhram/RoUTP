e36caef79f1497fba3ce7acea7da737a87368c86
Shaun Budhram <sbudhram@apple.com>
2017-01-13 00:05:08 -0800

    Setting up as framework

---
+++ RoUTP/ROUTP.h
@@ -0,0 +1,19 @@
+//
+//  ROUTP.h
+//  ROUTP
+//
+//  Created by Shaun Budhram on 1/12/17.
+//  Copyright © 2017 Shaun Budhram. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ROUTP.
+FOUNDATION_EXPORT double ROUTPVersionNumber;
+
+//! Project version string for ROUTP.
+FOUNDATION_EXPORT const unsigned char ROUTPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ROUTP/PublicHeader.h>
+
+
